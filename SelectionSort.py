369f287efcc6497ba29e5d6f0776cfb9959f0d9f
fakhru <latsaheb@gmail.com>
2022-06-30 21:14:12 +0530

    Selection sort with class checkin

--- SelectionSort.py
+++ SelectionSort.py
@@ -20,6 +20,31 @@ for i in range(0,len(lArray)-1):
 print ("Printing after sorting {}".format(lArray))
 
 
+class SelectionSort:
+    def __init__(self, l_Array):
+        self.lInputArr = l_Array
+
+        for i in range(0, len(self.lInputArr) - 1):
+            min = i
+            for j in range(i + 1, len(self.lInputArr)):  # starting from 1
+                if self.lInputArr[j] < self.lInputArr[min]:
+                    min = j
+            if min != i:
+                self.fcSwapObjects(self.lInputArr,i,min)
+
+    def fcSwapObjects(self,l_InputArr,ldx,lmin):
+        l_InputArr[ldx] = l_InputArr[ldx] + l_InputArr[lmin]
+        l_InputArr[lmin] = l_InputArr[ldx] - l_InputArr[lmin]
+        l_InputArr[ldx] = l_InputArr[ldx] - l_InputArr[lmin]
+        return l_InputArr
+
+sort1 = SelectionSort([7,10,11,4,3,190,1])
+print(sort1.lInputArr)
+sort2 = SelectionSort([7,8,1,20,30,100])
+print(sort2.lInputArr)
+
+
+
 
 
 
