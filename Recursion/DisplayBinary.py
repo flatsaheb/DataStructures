71e23b5cb92202bc676f6da9d38b793953021256
fakhru <latsaheb@gmail.com>
2022-07-03 12:39:15 +0530

    This function would display binary of any decimal numbers. Initial checkin

---
+++ Recursion/DisplayBinary.py
@@ -0,0 +1,9 @@
+def fDisplayBinary(n):
+    a = ''
+    while n>0:
+      lQuotient,lRemainder = divmod(n,2)
+      n = lQuotient
+      a +=str(lRemainder)xxx
+    return a[::-1] # reverse the string at the end
+n = int(input("Enter the number you want to have binary display \n"))
+print(fDisplayBinary(n))
