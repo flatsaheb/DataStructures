6d125fc9ebfc66f5382e37616cfbede85be56322
fakhru <latsaheb@gmail.com>
2022-07-03 12:40:42 +0530

    Corrected typo. V1.1

--- Recursion/DisplayBinary.py
+++ Recursion/DisplayBinary.py
@@ -3,7 +3,7 @@ def fDisplayBinary(n):
     while n>0:
       lQuotient,lRemainder = divmod(n,2)
       n = lQuotient
-      a +=str(lRemainder)xxx
+      a +=str(lRemainder)
     return a[::-1] # reverse the string at the end
 n = int(input("Enter the number you want to have binary display \n"))
 print(fDisplayBinary(n))
