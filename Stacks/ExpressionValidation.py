017d50cf83fa9de772db6a8ecae922b0a9dccd2d
fakhru <latsaheb@gmail.com>
2022-07-10 12:33:59 +0530

    Included main and CPyStack class. Modified the comment section

--- Stacks/ExpressionValidation.py
+++ Stacks/ExpressionValidation.py
@@ -1,88 +1,62 @@
+'''
+Expression Validation program would check the validity of expression
+by checking the corresponding opening brace has closing brace. Stack data-structure
+has been used to process the validation. Class CPyStack containing
+basic stack operations has been used.
+'''
+
+import CPyStack
+
 lStack = []
-top = None
+element = CPyStack.CStack(lStack)
 
-def fIsEmpty(stk):
-    if (stk==[]):
-        return True
-    else:
-        return False
+# Class to implement validation function.
+class CExprTest:
+    def __init__(self,l_ExprArray):
+        self.lExpr = l_ExprArray
 
-def fPush(stk,item):
-    stk.append(item)
-    top = len(stk)
 
-def fPop(stk):
-    if (fIsEmpty(stk)):
-        return ('Underflow')
-    else:
-        i = stk.pop()
-        if (len(stk) == 0):
-            top = None
-        else:
-            top = len(stk) -1
-        return i
+    def fMatchChar(self,l_InitalChar,l_TerminalChar):
+        if (l_InitalChar == '(' and l_TerminalChar == ')'):
+            return 1
+        elif (l_InitalChar == '{'and l_TerminalChar == '}'):
+            return 1
+        elif (l_InitalChar == '[' and l_TerminalChar == ']'):
+         return 1
+        else :
+         return 0
 
-def fPeek(stk):
-    if (fIsEmpty(stk)):
-        return 'Underfow'
-    else:
-        top = len(stk) - 2
-        return(stk[top])
 
-def fDisplay(stk):
-    if (fIsEmpty(stk)):
-        print ('Stack is Empty')
-    else:
-        top = len(stk)-1
-        print(stk[top],'<---top')
-        for i in range(top-1,-1,-1):
-            print(stk[i])
-
-def fMatchChar(l_InitalChar,l_TerminalChar):
-    if (l_InitalChar == '(' and l_TerminalChar == ')'):
-        return 1
-    elif (l_InitalChar == '{'and l_TerminalChar == '}'):
-        return 1
-    elif (l_InitalChar == '[' and l_TerminalChar == ']'):
-        return 1
-    else :
-        return 0
-def fCheckExpression(l_ExprArray):
-    for i in range(0,len(l_ExprArray)):
-        if (l_ExprArray[i] == '(' or l_ExprArray[i] == '{' or l_ExprArray[i] == '['):
-            fPush(lStack,l_ExprArray[i])
-        if (l_ExprArray[i] == ')' or l_ExprArray[i] == '}' or l_ExprArray[i] == ']'):
-            if fIsEmpty(lStack):
-                print('Error: Right symbols are more than Left')
-                return 0
-            else:
-                lTempChar = fPop(lStack)
-                if (not fMatchChar(lTempChar,l_ExprArray[i])):
-                    print('Error: Unmatched symbol found')
+    def fCheckExpression(self,l_ExprArray):
+        for i in range(0,len(l_ExprArray)):
+            if (l_ExprArray[i] == '(' or l_ExprArray[i] == '{' or l_ExprArray[i] == '['):
+                element.fPush(lStack,l_ExprArray[i])
+            if (l_ExprArray[i] == ')' or l_ExprArray[i] == '}' or l_ExprArray[i] == ']'):
+                if element.fIsEmpty(lStack):
+                    print('Error: Right symbols are more than Left')
                     return 0
-    if(fIsEmpty(lStack)):
-        print('Stack is empty all open and close paranthesis match')
-        return 1
-    else:
-        print('Stack is not empty open and close paranthesis not matching')
-        return 0
+                else:
+                    lTempChar = element.fPop(lStack)
+                    if (not self.fMatchChar(lTempChar,l_ExprArray[i])):
+                        print('Error: Unmatched symbol found')
+                        return 0
 
+        if(element.fIsEmpty(lStack)):
+            print('Stack is empty all open and close parenthesis match')
+            return 1
+        else:
+            print('Stack is not empty open and close parenthesis not matching')
+            return 0
 
-lExprArray = list("(a+b)*({c-d})")
-lValidity = fCheckExpression(lExprArray)
-if (lValidity == 1):
-    print("Expression is Valid")
-if (lValidity == 0):
-    print("Expression is invalid")
+def main():
+    lExpression = list("(a+b)*(c-d)")
+    lExprObj = CExprTest(lExpression)
+    lValidity = lExprObj.fCheckExpression(lExpression)
 
+    if (lValidity == 1):
+        print("Expression is Valid")
+    if (lValidity == 0):
+        print("Expression is invalid")
 
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
