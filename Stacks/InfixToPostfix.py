d55b5221545de1d0ac0a48f59c71f9baf2cfe3d2
fakhru <latsaheb@gmail.com>
2022-07-12 21:06:02 +0530

    Added new comments. No functional change though.

--- Stacks/InfixToPostfix.py
+++ Stacks/InfixToPostfix.py
@@ -1,3 +1,7 @@
+'''
+Infix to Postfix Conversion. Stack class CStack would be used for stack operations. isalpha function would
+be used to check whether the processing character is operand.
+'''
 from curses.ascii import isalpha
 import CPyStack
 
@@ -16,9 +20,12 @@ def fOutputPostfix(l_Array):
     lPostFix = ''
     lPoppedElement = ''
 
+    # Scan the string using for loop. String would pass as an array.
     for i in range(0, len(l_Array)):
+        # If it is an operand output it to expression.
         if isalpha(l_Array[i]):
             lPostFix = lPostFix + l_Array[i]
+        # Else If it is a left opening bracket then Pop all the elements and add them to expression
         elif l_Array[i] == ")":
            while True:
                 lPoppedElement = OElement.fPop(lStack)
@@ -26,20 +33,25 @@ def fOutputPostfix(l_Array):
                     break
                 else:
                     lPostFix = lPostFix + lPoppedElement
+        # Else If it is a right opening bracket then simply push to stack.
         elif l_Array[i] == "(":
-            OElement.fPush(lStack,l_Array[i] )
+            OElement.fPush(lStack,l_Array[i])
+
+        # Else  Compare the priorities of operands, if priority of operand on top of the stack is higher
+        # than one being processed then pop the item from stack and add to expression.
+        # Lastly push the processed operand.
         else:
             if OElement.fPeek(lStack) != 'Underflow':
                  if fPriority(OElement.fPeek(lStack)) >= fPriority(l_Array[i]):
                      lPoppedElement = OElement.fPop(lStack)
                      lPostFix = lPostFix + lPoppedElement
             OElement.fPush(lStack, l_Array[i])
-
+    # When entire infix string processed then pop the items from stack and add to the
+    # expression.
     while not OElement.fIsEmpty(lStack):
         lPostFix = lPostFix + OElement.fPop(lStack)
     return lPostFix
 
 
 print(fOutputPostfix(list("A*B-(C+D)+E")))
-#print(fOutputPostfix(list("A*(B+C)-D")))
 print(fOutputPostfix(list("A*(B+C)")))
